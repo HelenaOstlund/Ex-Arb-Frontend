6fad7912fe5864f33ac8f8a2e994a172cfa671ad
HelenaOstlund <ostlund_helena@hotmail.com>
2023-05-05 17:06:15 +0200

    CreateUnit.js made and implemented in AdminPage

--- src/Pages/Admin.js
+++ src/Pages/Admin.js
@@ -2,6 +2,7 @@ import React from "react";
 import Register from '../Components/Register';
 import LogoutButton from "../Components/LogoutButton";
 import DeleteUser from "../Components/DeleteUser";
+import CreateUnit from "../Components/CreateUnit";
 export default function Admin() {
 
     return (
@@ -9,6 +10,7 @@ export default function Admin() {
             <h1>AdminPage</h1>
             <Register />
             <DeleteUser />
+            <CreateUnit />
             <LogoutButton />
         </div>
     );
